b2668a54bcc6f25a92175fe55d98cb5f02c62600
Patrick Casey <patrick.casey1@outlook.com>
2026-05-07 23:19:51 -0400

    enable nvidia support
    Signed-off-by: Patrick Casey <patrick.casey1@outlook.com>

--- configuration.nix
+++ configuration.nix
@@ -20,8 +20,23 @@
 
   nixpkgs.config.allowUnfree = true;
 
+  hardware.graphics = {
+    enable = true;
+  };
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+    open = false;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+  nixpkgs.config.nvidia.acceptLicense = true;
+
   services.xserver = {
     enable = true;
+    videoDrivers = [ "nvidia" ];
     desktopManager = {
       xterm.enable = false;
     };
